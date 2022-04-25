16e464ce86ac1b528c4bbaa9d526b86500b2a05a
Adlet <eypk777@gmail.com>
2022-04-25 15:17:14 +0900

    둘러싸는 Plane 만듦

--- modules/assignments/assignment2.js
+++ modules/assignments/assignment2.js
@@ -11,7 +11,7 @@ export default class Assignment2 extends cs380.BaseApp {
     const { width, height } = gl.canvas.getBoundingClientRect();
     const aspectRatio = width / height;
     this.camera = new cs380.Camera();
-    vec3.set(this.camera.transform.localPosition, 0, 0, 5);
+    vec3.set(this.camera.transform.localPosition, 0, 0, 50);
     mat4.perspective(
       this.camera.projectionMatrix,
       glMatrix.toRadian(45),
@@ -22,11 +22,69 @@ export default class Assignment2 extends cs380.BaseApp {
 
     this.thingsToClear = [];
 
+    // SimpleOrbitControl
+    const orbitControlCenter = vec3.fromValues(0, 0, 0);
+    this.simpleOrbitControl = new cs380.utils.SimpleOrbitControl(
+      this.camera,
+      orbitControlCenter
+    );
+    this.thingsToClear.push(this.simpleOrbitControl);
+
+    // Mesh & Shader
+    const sphereMeshData = cs380.primitives.generateSphere();
+    const sphereMesh = cs380.Mesh.fromData(sphereMeshData);
+    const simpleShader = await cs380.buildShader(SimpleShader);
+    this.thingsToClear.push(sphereMesh, simpleShader);
+
+    // Generate Plane
+    this.planeX = 30
+    this.planeY = 30
+    this.planeZ = 30
+    this.planeColorStr = "#888888"
+    const planeBackMesh = cs380.Mesh.fromData(cs380.primitives.generatePlane(this.planeX, this.planeY));
+    const planeLeftMesh = cs380.Mesh.fromData(cs380.primitives.generatePlane(this.planeZ, this.planeY));
+    const planeRightMesh = cs380.Mesh.fromData(cs380.primitives.generatePlane(this.planeY, this.planeZ));
+    const planeBottomMesh = cs380.Mesh.fromData(cs380.primitives.generatePlane(this.planeX, this.planeY));
+    
+    this.planeBack = new cs380.RenderObject(planeBackMesh, simpleShader);
+    quat.rotateX(this.planeBack.transform.localRotation, this.planeBack.transform.localRotation, Math.PI);
+    vec3.set(this.planeBack.transform.localPosition, 0, 0, - this.planeZ / 2);
+    this.planeBack.uniforms.mainColor = vec3.create();
+    cs380.utils.hexToRGB(this.planeBack.uniforms.mainColor, this.planeColorStr);
+
+    this.planeLeft = new cs380.RenderObject(planeLeftMesh, simpleShader);
+    quat.rotateY(this.planeLeft.transform.localRotation, this.planeLeft.transform.localRotation, - Math.PI / 2);
+    vec3.set(this.planeLeft.transform.localPosition, - this.planeX / 2, 0, 0);
+    this.planeLeft.uniforms.mainColor = vec3.create();
+    cs380.utils.hexToRGB(this.planeLeft.uniforms.mainColor, this.planeColorStr);
+
+    this.planeRight = new cs380.RenderObject(planeRightMesh, simpleShader);
+    quat.rotateY(this.planeRight.transform.localRotation, this.planeRight.transform.localRotation, Math.PI / 2);
+    vec3.set(this.planeRight.transform.localPosition, this.planeX / 2, 0, 0);
+    this.planeRight.uniforms.mainColor = vec3.create();
+    cs380.utils.hexToRGB(this.planeRight.uniforms.mainColor, this.planeColorStr);
+
+    this.planeBottom = new cs380.RenderObject(planeBottomMesh, simpleShader);
+    quat.rotateX(this.planeBottom.transform.localRotation, this.planeBottom.transform.localRotation, Math.PI / 2);
+    vec3.set(this.planeBottom.transform.localPosition, 0, - this.planeY / 2, 0);
+    this.planeBottom.uniforms.mainColor = vec3.create();
+    cs380.utils.hexToRGB(this.planeBottom.uniforms.mainColor, "#000000");
+    // Generate Plane End
+
     // initialize picking shader & buffer
     const pickingShader = await cs380.buildShader(cs380.PickingShader);
     this.pickingBuffer = new cs380.PickingBuffer();
     this.pickingBuffer.initialize(width, height);
     this.thingsToClear.push(pickingShader, this.pickingBuffer);
+
+    this.sphere = new cs380.PickableObject(
+      sphereMesh,
+      simpleShader,
+      pickingShader,
+      1
+    );
+    this.sphere.uniforms.mainColor = vec3.create();
+    cs380.utils.hexToRGB(this.sphere.uniforms.mainColor, "#00FF00");
 
     // Event listener for interactions
     this.handleKeyDown = (e) => {
@@ -84,6 +142,7 @@ export default class Assignment2 extends cs380.BaseApp {
 
   update(elapsed, dt) {
     // Updates before rendering here
+    this.simpleOrbitControl.update(dt);
 
     // Render picking information first
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.pickingBuffer.fbo);
@@ -94,14 +153,21 @@ export default class Assignment2 extends cs380.BaseApp {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // renderPicking() here
+    this.sphere.renderPicking(this.camera);
 
     // Render real scene
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
     gl.clearDepth(1.0);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // render() here
+    this.sphere.render(this.camera);
+    this.planeBack.render(this.camera);
+    this.planeLeft.render(this.camera);
+    this.planeRight.render(this.camera);
+    this.planeBottom.render(this.camera);
   }
 }
